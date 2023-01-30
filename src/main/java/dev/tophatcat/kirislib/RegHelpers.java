a3de97cf3b16e7ab01b659e12729bb337a630673
KiriCattus <kiri_cattus@icloud.com>
2023-01-30 20:19:43 +0000

    Port to 1.19.3

--- src/main/java/dev/tophatcat/kirislib/RegHelpers.java
+++ src/main/java/dev/tophatcat/kirislib/RegHelpers.java
@@ -24,7 +24,6 @@ import com.google.common.base.Suppliers;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.sounds.SoundEvent;
 import net.minecraft.world.item.BlockItem;
-import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
 
@@ -46,12 +45,12 @@ public class RegHelpers {
     }
 
     public static <T extends Block> Supplier<T> createBlockWithItem(
-        ResourceLocation identifier, CreativeModeTab group, Supplier<T> block, Map<ResourceLocation, Supplier<T>> blockMap,
+        ResourceLocation identifier, Supplier<T> block, Map<ResourceLocation, Supplier<T>> blockMap,
         Map<ResourceLocation, Supplier<Item>> itemMap) {
         var wrapped = Suppliers.memoize(block::get);
         blockMap.put(identifier, wrapped);
         createBasicItem(identifier, () -> new BlockItem(wrapped.get(),
-            new Item.Properties().tab(group)), itemMap);
+            new Item.Properties()), itemMap);
         return wrapped;
     }
 
@@ -63,7 +62,7 @@ public class RegHelpers {
     }
 
     public static SoundEvent createSound(ResourceLocation identifier, Map<SoundEvent, ResourceLocation> soundMap) {
-        var soundEvent = new SoundEvent(identifier);
+        var soundEvent = SoundEvent.createVariableRangeEvent(identifier);
         soundMap.put(soundEvent, identifier);
         return soundEvent;
     }
